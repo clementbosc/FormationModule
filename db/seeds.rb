7b235a99460e0e200ef717a0e60dc2be70ce9f7c
Yannick Chevalier <yannick.chevalier@irit.fr>
2017-02-06 17:02:05 +0100

    ajout de données de base

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Devoir.create(name: "CC" )
+Devoir.create(name: "CT" )
+
+User.create(firstname: "John", lastname: "Lennon", email: "jl@beattles.org", numero: "num1")
+
+User.create(firstname: "John", lastname: "Steed", email: "js@avengers.org", numero: "num2")
+
+User.create(firstname: "John", lastname: "Runner", email: "jr@scots.org", numero: "num3")
+
+User.create(firstname: "John", lastname: "English", email: "je@beettle.org", numero: "num4")
+
+Group.create(name: "A1.1")
+Group.create(name: "A1.2")
+Group.create(name: "A2.1")
+for i in 0..5
+  TravauxDirige.create(name: "td", number: i)
+  SeanceLaboratoire.create(name: "tp", number: i )
+end
+
+
